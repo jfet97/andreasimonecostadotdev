aed850a4b9017ab818df509014a5a275496de5da
Andrea Simone Costa <andrysimo1997@gmail.com>
2019-07-20 20:52:45 +0200

    maybe netlifycms does work

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,41 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'andreasimonecostadotdev',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          ...process.env.NODE_ENV === "production" ? [purgecss] : []
+        ],
+      },
+    },
+  }
 }
