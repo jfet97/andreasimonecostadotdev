a367014b8bd4fd5c0554358bea2a417fb1a36340
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-04-16 12:33:54 +0200

    change utterances color depending in the selected theme

---
+++ static/js/utts.js
@@ -0,0 +1,50 @@
+(() => {
+    const body = document.body;
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+
+
+    addEventListener('message', event => {
+        // attendo che si sia caricato utterances
+        if (event.origin !== 'https://utteranc.es') {
+            return;
+        }
+
+        setTimeout(() => {
+            if (localStorage.getItem("colorscheme")) {
+                setUttColor(localStorage.getItem("colorscheme") === "light" ? "dark" : "light")
+            } else if (body.classList.contains('colorscheme-light') || body.classList.contains('colorscheme-dark')) {
+                setUttColor(body.classList.contains("colorscheme-dark") ? "light" : "dark")
+            } else {
+                setUttColor(darkModeMediaQuery.matches ? "light" : "dark")
+            }
+        }, 0)
+        
+    });
+
+    darkModeToggle.addEventListener('click', () => {
+        const newColor = body.classList.contains("colorscheme-dark") ? "light" : "dark"
+        setUttColor(newColor)
+    });
+
+    darkModeMediaQuery.addListener((event) => {
+        setUttColor(event.matches ? "dark" : "light");
+    });
+
+
+    function setUttColor(color) {
+
+        const [utt] = [...document.querySelectorAll("iframe.utterances-frame")]
+
+        console.log(utt.contentWindow)
+
+        const message = {
+            type: 'set-theme',
+            theme: `github-${color}`,
+        };
+
+        utt.contentWindow.postMessage(message, 'https://utteranc.es');
+    }
+})();
+
