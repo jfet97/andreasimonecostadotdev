cb5e22e16230a264a1f3eb357b77be3e9f3a5e6b
Andrea Simone Costa <andrysimo1997@gmail.com>
2021-04-23 22:54:55 +0200

    fix bug

--- static/js/align_cent_article_cloning.js
+++ static/js/align_cent_article_cloning.js
@@ -1,9 +1,11 @@
 (() => {
     document.addEventListener("DOMContentLoaded", function () {
         const pre = [...document.querySelectorAll("pre")].find(p => p.innerText.includes(`[["a", 1], ["b", 2], ["c", {}]]`))
-        pre.style['text-align'] = "center"
-        pre.parentNode.style.display = "flex"
-        pre.parentNode.style['justify-content'] = "center"
+        if(pre) {
+            pre.style['text-align'] = "center"
+            pre.parentNode.style.display = "flex"
+            pre.parentNode.style['justify-content'] = "center"
+        }
     });
 })();
 
